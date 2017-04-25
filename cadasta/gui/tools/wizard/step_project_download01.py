065176a10a48369ddede094329f31c445c662de8
Dimas Ciputra <dimas.ciputra@gmail.com>
2017-04-25 09:48:29 +0800

    Add project details and add contacts button (#159)
    * Add project details and add contacts button
    
    * Fix header
    
    * Tidy up header
    
    * pep8
    
    * Remove contacts menu

--- cadasta/gui/tools/wizard/step_project_download01.py
+++ cadasta/gui/tools/wizard/step_project_download01.py
@@ -21,6 +21,9 @@ from cadasta.gui.tools.wizard.wizard_step import get_wizard_step_ui_class
 from cadasta.utilities.i18n import tr
 from cadasta.utilities.resources import resources_path
 from cadasta.utilities.utilities import Utilities
+from cadasta.model.contact import Contact
+from cadasta.gui.tools.cadasta_dialog import CadastaDialog
+from cadasta.gui.tools.widget.contact_widget import ContactWidget
 
 __copyright__ = "Copyright 2016, Cadasta"
 __license__ = "GPL version 3"
@@ -35,14 +38,21 @@ LOGGER = logging.getLogger('CadastaQGISPlugin')
 class StepProjectDownload01(WizardStep, FORM_CLASS):
     """Step 1 for project download."""
 
-    def __init__(self, parent=None):
+    def __init__(self, iface, parent=None):
         """Constructor.
+
+        :param iface: An interface instance that will be passed to this class
+            which provides the hook by which you can manipulate the QGIS
+            application at run time.
+        :type iface: QgsInterface
 
         :param parent: parent - widget to use as parent.
         :type parent: QWidget
         """
         super(StepProjectDownload01, self).__init__(parent)
+        self.iface = iface
         self.project_api = None
+        self.current_contacts = None
 
     def set_widgets(self):
         """Set all widgets on the tab."""
@@ -53,6 +63,9 @@ class StepProjectDownload01(WizardStep, FORM_CLASS):
         self.throbber_loader.setMovie(movie)
         movie.start()
         self.get_available_projects()
+        self.add_contact_button.setEnabled(False)
+        self.add_contact_button.clicked.connect(
+            self.add_to_contacts)
 
     def selected_project(self):
         """Get selected project from combo box.
@@ -145,8 +158,70 @@ class StepProjectDownload01(WizardStep, FORM_CLASS):
             self.project_description_label.setText(
                     self.tr(project['name'].encode('utf-8')))
 
+        if project['urls']:
+            project_urls = ''
+            for url in project['urls']:
+                project_urls += url + ' \n'
+
+            if not project_urls.isspace():
+                project_urls = project_urls[:-2]
+                self.project_url_label.setText(project_urls)
+            else:
+                self.project_url_label.setText('-')
+        else:
+            self.project_url_label.setText('-')
+
+        if project['contacts']:
+            project_contacts = ''
+            for contact in project['contacts']:
+                project_contacts += contact['name']
+                if contact['email']:
+                    project_contacts += ', ' + contact['email']
+                if contact['tel']:
+                    project_contacts += ', ' + contact['tel']
+                project_contacts += ' \n'
+
+            if project_contacts:
+                self.add_contact_button.setEnabled(True)
+                self.current_contacts = project['contacts']
+                project_contacts = project_contacts[:-2]
+                self.contact_information_label.setText(project_contacts)
+        else:
+            self.contact_information_label.setText('-')
+            self.add_contact_button.setEnabled(False)
+
+        if project['access']:
+            self.privacy_status_label.setText(project['access'].title())
+
     def get_available_projects(self):
         """Get available projects."""
         self.throbber_loader.setVisible(True)
         self.project_api = Project(
             on_finished=self.get_available_projects_finished)
+
+    def add_to_contacts(self):
+        """Open contacts dialog and add current selected contacts to db."""
+        if not self.current_contacts:
+            return
+
+        for contact in self.current_contacts:
+            contact_from_db = Contact.get_rows(
+                name=contact['name'],
+                phone=contact['tel'],
+                email=contact['email']
+            )
+            if not contact_from_db:
+                new_contact = Contact()
+                new_contact.name = contact['name']
+                new_contact.email = contact['email']
+                new_contact.phone = contact['tel']
+                new_contact.save()
+
+        # Open contact dialog
+        dialog = CadastaDialog(
+            iface=self.iface,
+            subtitle='Contact',
+            widget=ContactWidget()
+        )
+        dialog.show()
+        dialog.exec_()
